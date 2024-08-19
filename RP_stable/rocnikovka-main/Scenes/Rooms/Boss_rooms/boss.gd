02ae6c67f64ddd40ee4b1287df8866cd0521d6d9
DobiasPetr <petr.dobias@student.gyarab.cz>
2024-08-19 23:42:27 +0200

    boss remake

--- RP_stable/rocnikovka-main/Scenes/Rooms/Boss_rooms/boss.gd
+++ RP_stable/rocnikovka-main/Scenes/Rooms/Boss_rooms/boss.gd
@@ -2,5 +2,42 @@ extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var speed = 200
+var health = 15
+var direction = Vector2(1, 1)
+var hit_counter = 0
+
 func _process(delta):
-	animated_sprite.play
+	animated_sprite.play("default")
+
+func _physics_process(delta):
+	velocity = direction.normalized() * speed
+	move_and_slide()
+	
+	if hit_counter >= 5:
+		print("A") # TODO add wait function - head hurts...
+		hit_counter = 0
+
+func _on_up_body_entered(body):
+	direction.y = 1
+	hit_counter += 1
+
+func _on_down_body_entered(body):
+	direction.y = -1
+	hit_counter += 1
+
+func _on_left_body_entered(body):
+	direction.x = 1
+	hit_counter += 1
+
+func _on_right_body_entered(body):
+	direction.x = -1
+	hit_counter += 1
+
+func _on_hb_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print("au")
+	health -= 1
+	if health == 0:
+		get_tree().change_scene_to_file("res://Scenes/Start_menu.tscn")
+
+#TODO boss bouncing off of player. Boss does not deal damage
--- RP_stable/rocnikovka-main/Scripts/player.gd
+++ RP_stable/rocnikovka-main/Scripts/player.gd
@@ -7,17 +7,12 @@ var health = 3
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	
-	if health == 0:
-		print("ded")
-	
 	var input_direction = Vector2(
 		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
 		Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	)
 	
 	velocity = input_direction.normalized() * SPEED
-	
 	move_and_slide()
 	
 func _process(delta):
